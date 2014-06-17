7f113e0b17879ec04af04509b11a4b65617af316
Tony Tahmouch <ttahmouch@gmail.com>
2014-06-17 01:15:58 -0400

    modified:   app/controllers/dash.js modified:   app/views/dash.html
    Added a swiss army knife endpoint POST /dash.
    The dashboard now accepts an action in a form-urlencoded POST body.
    These actions allow for managing or creating APIs if the user is authenticated.
    The dashboard HTML template has multipla modes now.
    It can depict a dashboard with no APIs, multiple APIs, or a single API being managed.

--- app/controllers/dash.js
+++ app/controllers/dash.js
@@ -1,21 +1,76 @@
 module.exports.controller = function (app) {
-    var user = require('../models/user.js');
+    /**
+     * Module Dependencies.
+     */
+    var user = require('../models/user.js'),
+        docker = require('../docker.js'),
+        title = 'Dashboard',
+        author = 'Tony Tahmouch tony@hapi.co',
+        copyright = 'Copyright © 2014 Hapi, Inc.',
+        description = 'Hapi Dashboard.',
+        keywords = 'hypermedia, api, rest, restful, javascript, node, node.js, web, docker';
 
     app
+        .post('/dash', function (req, res, next) {
+            if (req.isAuthenticated()) {
+                var redirect = true;
+                if (!!req.body && typeof req.body === 'object') {
+                    switch (req.body.action) {
+                        case 'manage':
+                            redirect = false;
+                            user.getHapiById({ email: req.user.email }, req.body.hapi, function (err, hapi) {
+                                if (!!hapi) {
+                                    docker.inspectContainer('/var/docker.sock', hapi.id, function (container) {
+                                        res.render('dash.html', {
+                                            layout: false,
+                                            title: title,
+                                            author: author,
+                                            copyright: copyright,
+                                            description: description,
+                                            keywords: keywords,
+                                            hapi: hapi,
+                                            running: !!container ? container.State.Running : null
+                                        });
+                                    });
+                                } else {
+                                    res.redirect('/dash');
+                                }
+                            });
+                            break;
+                        case 'create':
+                            redirect = false;
+                            res.render('dash.html', {
+                                layout: false,
+                                title: title,
+                                author: author,
+                                copyright: copyright,
+                                description: description,
+                                keywords: keywords,
+                                hapis: []
+                            });
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (redirect) {
+                    res.redirect('/dash');
+                }
+            } else {
+                res.redirect('/');
+            }
+        })
         .get('/dash', function (req, res, next) {
             if (req.isAuthenticated()) {
-                user.getHapisForUser(req.user.email, function (err, model) {
-                    var hapis = [];
-                    if (err) {
-                        console.log(err);
-                    } else if (model) {
-                        hapis = model.hapis;
-                    }
+                user.getHapis({ email: req.user.email }, function (err, hapis) {
                     res.render('dash.html', {
                         layout: false,
-                        title: 'Dashboard',
-                        user: req.isAuthenticated(),
-                        hapis: hapis
+                        title: title,
+                        author: author,
+                        copyright: copyright,
+                        description: description,
+                        keywords: keywords,
+                        hapis: !!hapis ? hapis : []
                     });
                 });
             } else {
