20c0d1276295dc75efa0e4e1d952cbe968116dc7
Tony Tahmouch <ttahmouch@gmail.com>
2014-06-17 01:36:15 -0400

    modified:   app/models/user.js
    Made large code reorganization.
    Removed all ForUser methods in and replaced them with methods expecting a User Object.
    Previously they expected the email address of a User.
    Removed all methods dealing with port number management.
    Added methods to get all Hapis, get a Hapi, and remove a Hapi of a User.

--- app/models/user.js
+++ app/models/user.js
@@ -29,145 +29,166 @@ function hash(pwd, salt, fn) {
     }
 }
 
-var userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    salt: String,
-    hash: String,
-    github: {
-        id: String,
-        email: String,
-        name: String
-    },
-    hapis: []
-})
-    .static('signup', function (email, password, done) {
-        var that = this;
-        hash(password, function (err, salt, hash) {
-            var user = {
-                email: email,
-                salt: salt,
-                hash: hash
-            };
-            if (err) {
-                throw err;
-            }
-            that.create(user, function (err, user) {
+module.exports = exports = mongoose.model('User',
+    mongoose
+        .Schema({
+            firstName: String,
+            lastName: String,
+            email: String,
+            salt: String,
+            hash: String,
+            github: {
+                id: String,
+                email: String,
+                name: String
+            },
+            hapis: []
+        })
+        .static('signup', function (email, password, done) {
+            var that = this;
+            hash(password, function (err, salt, hash) {
+                var user = {
+                    email: email,
+                    salt: salt,
+                    hash: hash
+                };
                 if (err) {
                     throw err;
                 }
-                done(null, user);
+                that.create(user, function (err, user) {
+                    if (err) {
+                        throw err;
+                    }
+                    done(null, user);
+                });
             });
-        });
-    })
-    .static('isValidUserPassword', function (email, password, done) {
-        this.findOne({ email: email }, function (err, user) {
-            if (err) {
-                return done(err);
-            }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect email.' });
-            }
-            hash(password, user.salt, function (err, hash) {
+        })
+        .static('isValidUserPassword', function (email, password, done) {
+            this.findOne({ email: email }, function (err, user) {
                 if (err) {
                     return done(err);
                 }
-                if (hash == user.hash) {
-                    return done(null, user);
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect email.' });
                 }
-                done(null, false, {
-                    message: 'Incorrect password'
+                hash(password, user.salt, function (err, hash) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (hash == user.hash) {
+                        return done(null, user);
+                    }
+                    done(null, false, {
+                        message: 'Incorrect password'
+                    });
                 });
             });
-        });
-    })
-    .static('findOrCreateOAuthUser', function (profile, done) {
-        var that = this,
-            query = {};
+        })
+        .static('findOrCreateOAuthUser', function (profile, done) {
+            var that = this,
+                query = {};
 
-        query[profile.authOrigin + '.id'] = profile.id;
+            query[profile.authOrigin + '.id'] = profile.id;
 
-        that.findOne(query, function (err, user) {
-                if (err) {
-                    throw err;
+            that.findOne(query, function (err, user) {
+                    if (err) {
+                        throw err;
+                    }
+                    if (user) {
+                        done(null, user);
+                    } else {
+                        that.findOne({
+                            'email': profile.emails[0].value
+                        }, function (err, user) {
+                            if (err) {
+                                throw err;
+                            }
+                            if (user) {
+                                user['' + profile.authOrigin] = {};
+                                user['' + profile.authOrigin].id = profile.id;
+                                user['' + profile.authOrigin].email = profile.emails[0].value;
+                                user['' + profile.authOrigin].name = profile.displayName;
+
+                                user.save(function (err, user) {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    done(null, user);
+                                });
+                            } else {
+                                user = {
+                                    email: profile.emails[0].value,
+                                    firstName: profile.displayName.split(' ')[0],
+                                    lastName: profile.displayName.replace(
+                                        profile.displayName.split(' ')[0] + ' ', ''
+                                    )
+                                };
+                                user['' + profile.authOrigin] = {};
+                                user['' + profile.authOrigin].id = profile.id;
+                                user['' + profile.authOrigin].email = profile.emails[0].value;
+                                user['' + profile.authOrigin].name = profile.displayName;
+
+                                that.create(user, function (err, user) {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    done(null, user);
+                                });
+                            }
+                        });
+                    }
                 }
-                if (user) {
-                    done(null, user);
-                } else {
-                    that.findOne({
-                        'email': profile.emails[0].value
-                    }, function (err, user) {
-                        if (err) {
-                            throw err;
-                        }
-                        if (user) {
-                            user['' + profile.authOrigin] = {};
-                            user['' + profile.authOrigin].id = profile.id;
-                            user['' + profile.authOrigin].email = profile.emails[0].value;
-                            user['' + profile.authOrigin].name = profile.displayName;
-
-                            user.save(function (err, user) {
-                                if (err) {
-                                    throw err;
-                                }
-                                done(null, user);
-                            });
-                        } else {
-                            user = {
-                                email: profile.emails[0].value,
-                                firstName: profile.displayName.split(' ')[0],
-                                lastName: profile.displayName.replace(
-                                    profile.displayName.split(' ')[0] + ' ', ''
-                                )
-                            };
-                            user['' + profile.authOrigin] = {};
-                            user['' + profile.authOrigin].id = profile.id;
-                            user['' + profile.authOrigin].email = profile.emails[0].value;
-                            user['' + profile.authOrigin].name = profile.displayName;
-
-                            that.create(user, function (err, user) {
-                                if (err) {
-                                    throw err;
-                                }
-                                done(null, user);
-                            });
-                        }
-                    });
-                }
-            }
-        );
-    })
-    .static('addHapiForUser', function (email, hapi) {
-        var that = this;
-        that.findOneAndUpdate(
-            {
-                email: email
-            },
-            {
+            );
+        })
+        .static('addHapi', function (user, hapi, done) {
+            this.findOneAndUpdate(user, {
                 $push: {
                     hapis: hapi
                 }
-            },
-            function (err, model) {
+            }, function (err, user) {
                 if (err) {
-                    console.log(err);
+                    return done(err);
+                } else if (user) {
+                    return done(null, hapi);
                 }
-                console.log(model);
-            }
-        );
-    })
-    .static('getHapisForUser', function (email, callback) {
-        var that = this;
-        that.findOne(
-            {
-                email: email
-            },
-            callback
-        );
-    })
-    .static('getLargestHapiPort', function (callback) {
-        var that = this;
-        that.find({}, callback);
-    });
-module.exports = exports = mongoose.model('User', userSchema);
+            });
+        })
+        .static('removeHapi', function (user, hapi, done) {
+            this.findOneAndUpdate(user, {
+                $pull: {
+                    hapis: hapi
+                }
+            }, function (err, user) {
+                if (err) {
+                    return done(err);
+                } else if (user) {
+                    return done(null, hapi);
+                }
+            });
+        })
+        .static('getHapiById', function (user, id, done) {
+            this.findOne(user, function (err, user) {
+                if (err) {
+                    return done(err);
+                } else if (user) {
+                    var hapis = user.hapis;
+                    for (var hapi in hapis) {
+                        hapi = hapis[hapi];
+                        if (hapi.id === id) {
+                            return done(null, hapi);
+                        }
+                    }
+                    return done(null, null);
+                }
+            });
+        })
+        .static('getHapis', function (user, done) {
+            this.findOne(user, function (err, user) {
+                if (err) {
+                    return done(err);
+                } else if (user) {
+                    return done(null, user.hapis);
+                }
+            });
+        })
+);
