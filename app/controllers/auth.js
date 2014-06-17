08f6dc66aa1f34a6dc011f893e10d73f223294d3
Tony Tahmouch <ttahmouch@gmail.com>
2014-06-17 01:22:23 -0400

    modified:   app/controllers/auth.js
    Added a swiss army knife endpoint, POST /auth.
    This endpoint accepts form-urlencoded body actions for various procedures.
    These procedures include logging in, logging out, registering, and logging in with Github.
    
    Added the ability to check for the application environment, development or production, to decide on OAuth client Id and Secret.
    
    Removed old endpoints that were consolidated into the POST endpoint.

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,30 +1,18 @@
 module.exports.controller = function (app) {
     /**
-     * Module dependencies.
+     * Module Dependencies.
      */
     var passport = require('passport'),
         passportGithub = require('passport-github'),
         passportLocal = require('passport-local'),
         user = require('../models/user.js');
+
     /**
-     * Production Github Application.
+     * Check Environment and Configure Passport.
      */
-    var githubProduction = {
-        clientID: '4e19f045b5dbe86fcc6e',
-        clientSecret: 'a21ca6f13a8a4e4575bf4579a8c317663880f685',
-        callbackURL: 'http://hapi.co/auth/github/callback'
-    };
-    /**
-     * Development Github Application.
-     */
-    var githubDevelopment = {
-        clientID: '87bb75f0ddb11a79ff68',
-        clientSecret: '7446b289245f51055b547af24fcb4b8ccdb50d9f',
-        callbackURL: 'http://0.0.0.0:8080/auth/github/callback'
-    };
-    /**
-     * Configure Passport.
-     */
+    var environment = app.get('environment'),
+        isDevEnvironment = environment === 'development';
+
     passport.serializeUser(function (user, done) {
         done(null, user.id);
     });
@@ -41,7 +29,13 @@ module.exports.controller = function (app) {
             user.isValidUserPassword(email, password, done);
         }
     ));
-    passport.use(new passportGithub.Strategy(githubDevelopment, function (accessToken, refreshToken, profile, done) {
+    passport.use(new passportGithub.Strategy({
+            clientID: isDevEnvironment ? '87bb75f0ddb11a79ff68' : '4e19f045b5dbe86fcc6e',
+            clientSecret: isDevEnvironment
+                ? '7446b289245f51055b547af24fcb4b8ccdb50d9f'
+                : 'a21ca6f13a8a4e4575bf4579a8c317663880f685',
+            callbackURL: isDevEnvironment ? 'http://0.0.0.0:8080/auth/github' : 'http://hapi.co/auth/github'
+        }, function (accessToken, refreshToken, profile, done) {
             profile.authOrigin = 'github';
             user.findOrCreateOAuthUser(profile, function (err, user) {
                 return done(err, user);
@@ -49,41 +43,62 @@ module.exports.controller = function (app) {
         }
     ));
 
+    var githubAuthentication = passport.authenticate('github', { scope: 'email' }),
+        localAuthentication = passport.authenticate('local', { successRedirect: '/dash', failureRedirect: '/' });
+
     app
         .get('/auth/github', passport.authenticate('github', {
-            scope: 'email'
-        }))
-        .get('/auth/github/callback', passport.authenticate('github', {
             successRedirect: '/dash',
             failureRedirect: '/'
         }))
-        .post('/signin', passport.authenticate('local', {
-            successRedirect: '/dash',
-            failureRedirect: '/'
-        }))
-        .get('/logout', function (req, res, next) {
-            req.logout();
-            res.redirect('/');
-        })
-        .post('/signup', function (req, res, next) {
-            user.count({
-                email: req.body.email
-            }, function (err, count) {
-                if (count === 0) {
-                    user.signup(req.body.email, req.body.password, function (err, user) {
-                        if (err) {
-                            throw err;
-                        }
-                        req.login(user, function (err) {
-                            if (err) {
-                                return next(err);
+        .post('/auth', function (req, res, next) {
+            if (!!req.body && typeof req.body === 'object') {
+                var action = req.body.action,
+                    email = req.body.email,
+                    password = req.body.password;
+
+                console.log(action);
+                console.log(email);
+                console.log(password);
+
+                switch (action) {
+                    case 'login':
+                        localAuthentication(req, res, next);
+                        break;
+                    case 'register':
+                        user.count({
+                            email: email
+                        }, function (err, count) {
+                            if (count === 0) {
+                                user.signup(email, password, function (err, user) {
+                                    if (err) {
+                                        return next(err);
+                                    }
+                                    req.login(user, function (err) {
+                                        if (err) {
+                                            return next(err);
+                                        }
+                                        return res.redirect('/dash');
+                                    });
+                                });
+                            } else {
+                                return res.redirect('/');
                             }
-                            return res.redirect('/dash');
                         });
-                    });
-                } else {
-                    res.redirect('/');
+                        break;
+                    case 'github':
+                        githubAuthentication(req, res, next);
+                        break;
+                    case 'logout':
+                        req.logout();
+                        res.redirect('/');
+                        break;
+                    default:
+                        res.redirect('/dash');
+                        break;
                 }
-            });
+            } else {
+                res.redirect('/dash');
+            }
         });
 };
